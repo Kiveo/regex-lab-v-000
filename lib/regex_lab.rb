66de92dd45ecf43fdf7538c212034e8750a00b80
Maverick Tesini <mavericktesini@gmail.com>
2018-03-13 15:13:19 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/^[un]\/[ing]z/).length
+  x = text.scan(/^[un]\/[ing]z/)
+  x
 end
 
 def words_five_letters_long(text)
